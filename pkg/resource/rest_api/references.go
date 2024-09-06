27d94b8bedd176586b9e97af275f862b82a3d040
Amine <hilalyamine@gmail.com>
2024-09-05 23:49:04 -0700

    Release artifacts for release v0.0.1 (#13)
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/rest_api/references.go
+++ pkg/resource/rest_api/references.go
@@ -142,12 +142,8 @@ func getReferencedResourceState_VPCEndpoint(
 	if err != nil {
 		return err
 	}
-	var refResourceSynced, refResourceTerminal bool
+	var refResourceTerminal bool
 	for _, cond := range obj.Status.Conditions {
-		if cond.Type == ackv1alpha1.ConditionTypeResourceSynced &&
-			cond.Status == corev1.ConditionTrue {
-			refResourceSynced = true
-		}
 		if cond.Type == ackv1alpha1.ConditionTypeTerminal &&
 			cond.Status == corev1.ConditionTrue {
 			return ackerr.ResourceReferenceTerminalFor(
@@ -159,6 +155,13 @@ func getReferencedResourceState_VPCEndpoint(
 		return ackerr.ResourceReferenceTerminalFor(
 			"VPCEndpoint",
 			namespace, name)
+	}
+	var refResourceSynced bool
+	for _, cond := range obj.Status.Conditions {
+		if cond.Type == ackv1alpha1.ConditionTypeResourceSynced &&
+			cond.Status == corev1.ConditionTrue {
+			refResourceSynced = true
+		}
 	}
 	if !refResourceSynced {
 		return ackerr.ResourceReferenceNotSyncedFor(
